35a8d6e53637bfe7ccda32291e9408be1da14f72
Arye <dabergshoeff@gmail.com>
2020-12-01 16:30:55 +0100

    3D bottle movement implemented

--- Assets/Scripts/Bottle.cs
+++ Assets/Scripts/Bottle.cs
@@ -1,20 +1,70 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityAtoms;
+using UnityAtoms.BaseAtoms;
 using UnityEngine;
 
 public class Bottle : MonoBehaviour
 {
+    public FloatVariable MoveSpeed;
+    public FloatVariable MaxSpeed;
 
+    private Rigidbody myRigidbody;
+    private List<Ripple> myRipples;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRigidbody = GetComponent<Rigidbody>();
+        myRipples = new List<Ripple>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MoveBottle();
+    }
+
+    private void MoveBottle() {
+        foreach (Ripple r in myRipples) {
+            Vector3 direction = transform.position - r.transform.position;
+            Vector3 heading = direction.normalized;
+            myRigidbody.AddForce(heading * MoveSpeed.Value);
+        }
+
+        /*foreach (GameObject go in streams) {
+            myRigidbody.AddForce(-go.transform.up * 0.3f);
+        }*/
+
+        var v = myRigidbody.velocity;
+        if (v.sqrMagnitude > MaxSpeed.Value * MaxSpeed.Value) {
+            myRigidbody.velocity = v.normalized * MaxSpeed.Value;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (!other.CompareTag("Ripple"))
+            return;
+
+        Ripple r = other.GetComponent<Ripple>();
+        if (!myRipples.Contains(r)) {
+            myRipples.Add(r);
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (!other.CompareTag("Ripple"))
+            return;
+
+        Ripple r = other.GetComponent<Ripple>();
+        if (myRipples.Contains(r)) {
+            myRipples.Remove(r);
+        }
+    }
+
+    public void RippleRemoved(Ripple r) {
+        if (myRipples.Contains(r)){
+            myRipples.Remove(r);
+        }
     }
 }
--- Assets/Scripts/Ripple.cs
+++ Assets/Scripts/Ripple.cs
@@ -1,21 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityAtoms;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class Ripple : MonoBehaviour
 {
     public static RippleEvent DestroyEvent;
+    public RippleValueList AllRipples;
     public float totalSize = 1f;
     public float timeTillFull = 1f;
 
     private float startTime = 0f;
     private bool growing = false;
 
-    void Start() {
+    private void Awake() {
         DestroyEvent = new RippleEvent();
         startTime = Time.time;
         growing = true;
+        AllRipples.Add(this);
     }
 
     private void Update() {
@@ -33,6 +36,10 @@ public class Ripple : MonoBehaviour
             t = t / timeTillFull;
             transform.localScale = new Vector3(t * totalSize, t * totalSize, t * totalSize);
         }
+    }
+
+    private void OnDestroy() {
+        AllRipples.Remove(this);
     }
 }
 
