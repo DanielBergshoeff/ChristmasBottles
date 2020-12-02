243b625e8f5d28ab68ad9b67cbb81c465fc2bb35
Arye <dabergshoeff@gmail.com>
2020-12-02 09:50:26 +0100

    Added start and pause menu

---
+++ Assets/Scripts/Game/PauseMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject PausePanel;
+
+    private bool paused = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (!paused)
+                Pause();
+            else
+                UnPause();
+        }
+    }
+
+    public void Quit() {
+        Application.Quit();
+        EditorApplication.isPlaying = false;
+    }
+
+    public void Pause() {
+        Time.timeScale = 0f;
+        PausePanel.SetActive(true);
+        paused = true;
+    }
+
+    public void UnPause() {
+        Time.timeScale = 1f;
+        PausePanel.SetActive(false);
+        paused = false;
+    }
+}
