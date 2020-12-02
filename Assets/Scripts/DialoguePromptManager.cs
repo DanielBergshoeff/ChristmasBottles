9f3c796d60025ae59be9e3d39f218b53dc941813
Arye <dabergshoeff@gmail.com>
2020-12-02 11:55:46 +0100

    Rocks working

---
+++ Assets/Scripts/DialoguePromptManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityAtoms.BaseAtoms;
+using UnityEngine;
+
+public class DialoguePromptManager : MonoBehaviour
+{
+    public FloatReference TextSpeed;
+    public FloatReference StayTime;
+
+    private string currentDialogue;
+    private float dialogueTimer = 0f;
+    private TMPro.TextMeshProUGUI dialogueText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogueText = GetComponentInChildren<TMPro.TextMeshProUGUI>();
+        dialogueText.text = "";
+        currentDialogue = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentDialogue == "")
+            return;
+
+        dialogueTimer += (Time.deltaTime / currentDialogue.Length) * TextSpeed;
+        int l = (int)Mathf.Clamp(currentDialogue.Length * dialogueTimer, 1f, currentDialogue.Length);
+        dialogueText.text = currentDialogue.Substring(0, l);
+        if(currentDialogue.Length * dialogueTimer >= currentDialogue.Length + StayTime * TextSpeed) {
+            currentDialogue = "";
+            dialogueText.text = "";
+        }
+    }
+
+    public void StartDialogue(string d) {
+        dialogueTimer = 0f;
+        currentDialogue = d;
+    }
+}
---
+++ Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityAtoms.BaseAtoms;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public VoidEvent RestartGameEvent;
+
+    private void Start() {
+        RestartGameEvent.Register(RestartGame);
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.E)) {
+            RestartGame();
+        }
+    }
+
+    public void RestartGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    private void OnDestroy() {
+        RestartGameEvent.Unregister(RestartGame);
+    }
+}
