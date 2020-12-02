662725628b581aa4027eed40af1245746c898f87
Arye <dabergshoeff@gmail.com>
2020-12-02 16:56:33 +0100

    Improved fade system and added introduction

---
+++ Assets/Scripts/Introduction.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityAtoms.BaseAtoms;
+using UnityEngine;
+
+public class Introduction : MonoBehaviour
+{
+    public FloatEvent FadeInEvent;
+    public StringEvent DialogueEvent;
+    public VoidEvent InstantFadeEvent;
+
+    public Letter IntroductionText;
+    public float TimePerPart = 5f;
+    public float FadeInTime = 3f;
+    public float StartWaitTime = 3f;
+
+    private int currentIntroPart = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InstantFadeEvent.Raise();
+        Invoke("StartIntro", StartWaitTime);
+    }
+
+    private void StartIntro() {
+        if (IntroductionText != null) {
+            currentIntroPart = -1;
+            NextIntroPart();
+        }
+        else {
+            EndIntro();
+        }
+    }
+
+    private void NextIntroPart() {
+        currentIntroPart++;
+        if (currentIntroPart < IntroductionText.Parts.Count) {
+            DialogueEvent.Raise(IntroductionText.Parts[currentIntroPart]);
+            Invoke("NextIntroPart", TimePerPart);
+        }
+        else {
+            EndIntro();
+        }
+    }
+
+    private void EndIntro() {
+        FadeInEvent.Raise(FadeInTime);
+    }
+}
