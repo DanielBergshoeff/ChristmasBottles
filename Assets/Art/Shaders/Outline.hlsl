1fb563a99dc244d5babf044f2ffaa17c5bb61472
Ilja6104 <ilja.ham@gmail.com>
2020-12-03 15:06:59 +0100

    RippleShader

--- Assets/Art/Shaders/Outline.hlsl
+++ Assets/Art/Shaders/Outline.hlsl
@@ -1,55 +1,42 @@
-﻿TEXTURE2D(_CameraDepthTexture);
-SAMPLER(sampler_CameraDepthTexture);
-float4 _CameraDepthTexture_TexelSize;
+﻿#ifndef SOBELOUTLINES_INCLUDED
+#define SOBELOUTLINES_INCLUDED
 
-TEXTURE2D(_CameraDepthNormalsTexture);
-SAMPLER(sampler_CameraDepthNormalsTexture);
- 
-float3 DecodeNormal(float4 enc)
+#endif
+// These are points to sample relative to the starting point
+static float2 sobelSamplePoints[9] =
 {
-    float kScale = 1.7777;
-    float3 nn = enc.xyz*float3(2*kScale,2*kScale,0) + float3(-kScale,-kScale,1);
-    float g = 2.0 / dot(nn.xyz,nn.xyz);
-    float3 n;
-    n.xy = g*nn.xy;
-    n.z = g-1;
-    return n;
-}
+    float2(-1, 1), float2(0, 1), float2(1, 1),
+    float2(-1, 0), float2(0, 0), float2(1, 1),
+    float2(-1, -1), float2(0, -1), float2(1, -1),
+};
 
-void OutlineObject_float(float2 UV, float OutlineThickness, float DepthSensitivity, float NormalsSensitivity, out float Out)
+// Weights for the x component
+static float sobelXMatrix[9] =
 {
-    float halfScaleFloor = floor(OutlineThickness * 0.5);
-    float halfScaleCeil = ceil(OutlineThickness * 0.5);
-    
-    float2 uvSamples[4];
-    float depthSamples[4];
-    float3 normalSamples[4];
+    1, 0, -1,
+    2, 0, -2,
+    1, 0, -1
+};
 
-    uvSamples[0] = UV - float2(_CameraDepthTexture_TexelSize.x, _CameraDepthTexture_TexelSize.y) * halfScaleFloor;
-    uvSamples[1] = UV + float2(_CameraDepthTexture_TexelSize.x, _CameraDepthTexture_TexelSize.y) * halfScaleCeil;
-    uvSamples[2] = UV + float2(_CameraDepthTexture_TexelSize.x * halfScaleCeil, -_CameraDepthTexture_TexelSize.y * halfScaleFloor);
-    uvSamples[3] = UV + float2(-_CameraDepthTexture_TexelSize.x * halfScaleFloor, _CameraDepthTexture_TexelSize.y * halfScaleCeil);
+// Weights for the y component
+static float sobelYMatrix[9] =
+{
+    1, 2, 1,
+    0, 0, 0,
+    -1, -2, -1
+};
 
-    for(int i = 0; i < 4 ; i++)
+void DepthSobel_float(float2 UV, float Thickness, out float Out)
+{
+    float2 sobel = 0;
+    // We can unroll this loop to make it more efficient
+    // The compiler is also smart enough to remove the i=4 iteration, which is always zero
+    [unroll]
+    for (int i = 0; i < 9; i++)
     {
-        depthSamples[i] = SAMPLE_TEXTURE2D(_CameraDepthTexture, sampler_CameraDepthTexture, uvSamples[i]).r;
-        normalSamples[i] = DecodeNormal(SAMPLE_TEXTURE2D(_CameraDepthNormalsTexture, sampler_CameraDepthNormalsTexture, uvSamples[i]));
+        float depth = SHADERGRAPH_SAMPLE_SCENE_DEPTH(UV + sobelSamplePoints[i] * Thickness);
+        sobel += depth * float2(sobelXMatrix[i], sobelYMatrix[i]);
     }
-
-    // Depth
-    float depthFiniteDifference0 = depthSamples[1] - depthSamples[0];
-    float depthFiniteDifference1 = depthSamples[3] - depthSamples[2];
-    float edgeDepth = sqrt(pow(depthFiniteDifference0, 2) + pow(depthFiniteDifference1, 2)) * 100;
-    float depthThreshold = (1/DepthSensitivity) * depthSamples[0];
-    edgeDepth = edgeDepth > depthThreshold ? 1 : 0;
-
-    // Normals
-    float3 normalFiniteDifference0 = normalSamples[1] - normalSamples[0];
-    float3 normalFiniteDifference1 = normalSamples[3] - normalSamples[2];
-    float edgeNormal = sqrt(dot(normalFiniteDifference0, normalFiniteDifference0) + dot(normalFiniteDifference1, normalFiniteDifference1));
-    edgeNormal = edgeNormal > (1/NormalsSensitivity) ? 1 : 0;
-
-    float edge = max(edgeDepth, edgeNormal);
-    Out = edge;
+    // Get the final sobel value
+    Out = length(sobel);
 }
-
